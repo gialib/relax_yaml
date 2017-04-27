172f56bec204b2530d91a64e7eb66d45eb31e3aa
WangJinzhong <andywang7259@163.com>
2017-04-27 20:28:56 +0800

    fix appliaction

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RelaxYaml.Mixfile do
 
   def project do
     [app: :relax_yaml,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -18,7 +18,8 @@ defmodule RelaxYaml.Mixfile do
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: [:yamerl]
     ]
   end
 
