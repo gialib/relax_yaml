27ccf2d4a623c93c88ec903f602ab4e516a99b52
WangJinzhong <andywang7259@163.com>
2017-04-28 11:40:50 +0800

    char list to string

--- lib/relax_yaml/decoder.ex
+++ lib/relax_yaml/decoder.ex
@@ -6,27 +6,33 @@ defmodule RelaxYaml.Decoder do
 
   def read_all_from_file(path, options \\ []) do
     path
-      |> :yamerl_constr.file(@yamerl_options)
-      |> RelaxYaml.Mapper.process(options)
+    |> :yamerl_constr.file(@yamerl_options)
+    |> RelaxYaml.Mapper.process(options)
   end
 
   def read_from_file(path, options \\ []) do
     path
-      |> :yamerl_constr.file(@yamerl_options)
-      |> List.last()
-      |> RelaxYaml.Mapper.process(options)
+    |> :yamerl_constr.file(@yamerl_options)
+    |> List.last()
+    |> RelaxYaml.Mapper.process(options)
   end
 
   def read_all_from_string(string, options \\ []) do
     string
-      |> :yamerl_constr.string(@yamerl_options)
-      |> RelaxYaml.Mapper.process(options)
+    |> from_char_list
+    |> :yamerl_constr.string(@yamerl_options)
+    |> RelaxYaml.Mapper.process(options)
   end
 
   def read_from_string(string, options \\ []) do
     string
+    |> from_char_list
     |> :yamerl_constr.string(@yamerl_options)
     |> List.last()
     |> RelaxYaml.Mapper.process(options)
+  end
+
+  defp from_char_list(char_list) do
+    Enum.join(for <<c::utf8 <- char_list>>, do: <<c::utf8>>)
   end
 end
